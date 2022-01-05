7a87e3a59aaea43ddac4753cafe9abbff0667a45
lunnlew <4288315+lunnlew@users.noreply.github.com>
2022-01-05 13:23:02 +0000

    Deploying to gh-pages from  @ 68c5eb9d31b454e911fab8ce91393c9a37d56b44 🚀

---
+++ assets/Home.2eeb445c.js
@@ -0,0 +1,11 @@
+import{d as n,h as c,b as a,e as d,g as e,i as o,u as t}from"./vendor.320141b3.js";var p=`npm install --save baidu-map-vue3
+// \u6216\u8005
+yarn add baidu-map-vue3`,i=`import { createApp } from 'vue'
+import App from './App.vue'
+import BaiduMapVue3 from 'baidu-map-vue3'
+
+const app = createApp(App)
+app.use(BaiduMapVue3)
+app.mount('#app')`,u=`<script setup lang="ts">
+import { BaiduMap } from 'baidu-map-vue3'
+<\/script>`;const l={class:"page-content"},_={class:"code"},r=e("div",{class:"code-title"},[e("span",null,"\u5B89\u88C5")],-1),v={class:"code-content"},m={class:"code-snippet"},h=e("div",{class:"code-title"},[e("span",null,"\u5168\u5C40\u5F15\u5165")],-1),f={class:"code-content"},g={class:"code-snippet"},b=e("div",{class:"code-title"},[e("span",null,"\u7EC4\u4EF6\u5F15\u5165")],-1),B={class:"code-content"},A={class:"code-snippet"},y=n({setup(C){return(V,j)=>{const s=c("highlightjs");return a(),d("div",l,[e("div",_,[r,e("div",v,[e("div",m,[o(s,{code:t(p)},null,8,["code"])])]),h,e("div",f,[e("div",g,[o(s,{code:t(i)},null,8,["code"])])]),b,e("div",B,[e("div",A,[o(s,{code:t(u)},null,8,["code"])])])])])}}});export{y as default};
