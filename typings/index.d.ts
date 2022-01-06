870d897ae3001ae02654ee2c1c5f260a06320864
lunnlew <4288315+lunnlew@users.noreply.github.com>
2022-01-06 11:14:11 +0800

    Marker组件增加属性及变更监听

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -351,6 +351,19 @@ declare namespace BaiduMapVue3 {
          */
         title?: string
         /**
+         * 文本标注内容
+         */
+        label?: string
+        /**
+         * 文本标注选项
+         */
+        labelOptions?: BMapGL.LabelOptions
+        /**
+         * 覆盖物的zIndex
+         * @param zIndex
+         */
+        zIndex?: number
+        /**
          * 是否显示
          */
         show?: boolean
@@ -1964,7 +1977,7 @@ declare namespace BaiduMapVue3 {
              * @param content
              * @param opts
              */
-            constructor(content: string, opts: LabelOptions)
+            constructor(content: string, opts?: LabelOptions)
             /**
              * 设置文本标注样式，该样式将作用于文本标注的容器元素上。其中styles为JavaScript对象常量，比如： setStyle({ color : "red", fontSize : "12px" }) 注意：如果css的属性名中包含连字符，需要将连字符去掉并将其后的字母进行大写处理，例如：背景色属性要写成：backgroundColor
              * @param styles
@@ -6605,7 +6618,7 @@ declare namespace BaiduMapVue3 {
              * @param content String
              * @param opts LabelOptions
              */
-            Label: { new(content: string, opts: LabelOptions): Label }
+            Label: { new(content: string, opts?: LabelOptions): Label }
             /**
              * 创建折线覆盖物对象
              * @param points Array<Point>
