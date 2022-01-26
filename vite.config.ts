290329a198082e56ababb77c1a49eccb03a30392
lunnlew <4288315+lunnlew@users.noreply.github.com>
2022-01-26 10:58:26 +0800

    更改构建产出

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: 'production' | 'development' }) => {
     console.log('mode', mode)
     return defineConfig({
+        build: {
+            lib: {
+                entry: path.resolve(__dirname, 'packages/main.ts'),
+                name: 'baidu-map-vue3',
+                fileName: (format) => `lib.${format}.js`
+            },
+            rollupOptions: {
+                external: ['vue'],
+                output: {
+                    globals: {
+                        vue: 'Vue'
+                    }
+                }
+            }
+        },
         resolve: {
             extensions: ['.ts', '.tsx', '.js', '.vue', '.json', '.css'],
         },
